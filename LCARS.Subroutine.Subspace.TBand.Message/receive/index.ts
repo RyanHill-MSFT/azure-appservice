6395ccb6f8871c666d0ac6e7ad71fa62189df930
Ryan Hill <ryhill@microsoft.com>
2020-11-23 09:46:54 -0500

    naming cleanup

--- LCARS.Subroutine.Subspace.TBand.Message/receive/index.ts
+++ LCARS.Subroutine.Subspace.TBand.Message/receive/index.ts
@@ -1,6 +1,6 @@
 import { AzureFunction, Context } from "@azure/functions"
 
-const transmissionReceived: AzureFunction = async function transmissionReceived(context: Context, transmissionReceived: any) {
+const transmissionReceived: AzureFunction = async function (context: Context, transmissionReceived: any) {
     context.log(`Subspace messaged received from ${transmissionReceived.ConnectionId}`);
     context.log(`Message: ${context.bindingData.message}`);
 }
