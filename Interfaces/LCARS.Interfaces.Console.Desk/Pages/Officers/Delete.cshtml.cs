ddd089060853ff55fe1c872f2ff1ee49d7eaae98
Ryan Hill <ryhill@microsoft.com>
2021-04-11 23:06:44 -0400

    add desk console

---
+++ Interfaces/LCARS.Interfaces.Console.Desk/Pages/Officers/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LCARS.Core;
+using LCARS.Core.Data.Crew;
+
+namespace LCARS.Interfaces.Console.Desk.Pages.Officers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Databank _context;
+
+        public DeleteModel(Databank context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Officer Officer { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Officer = await _context.Officers.FirstOrDefaultAsync(m => m.SerialNo == id);
+
+            if (Officer == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Officer = await _context.Officers.FindAsync(id);
+
+            if (Officer != null)
+            {
+                _context.Officers.Remove(Officer);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Interfaces/LCARS.Interfaces.Console.Desk/Pages/Officers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LCARS.Core;
+using LCARS.Core.Data.Crew;
+
+namespace LCARS.Interfaces.Console.Desk.Pages.Officers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Databank _context;
+
+        public IndexModel(Databank context)
+        {
+            _context = context;
+        }
+
+        public IList<Officer> Officer { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Officer = await _context.Officers.ToListAsync();
+        }
+    }
+}
