70931f8253d96bbfd84902319bf4e2c9ce007281
Ryan Hill <ryhill@microsoft.com>
2021-04-14 09:31:36 -0400

    add storage-file-datalake package

--- LCARS/LCARS.Command.Sequencer.RhoUpsilon/TransporterSiteCoordinates/__init__.py
+++ LCARS/LCARS.Command.Sequencer.RhoUpsilon/TransporterSiteCoordinates/__init__.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 import azure.functions as func
-
+from azure.storage.filedatalake import DataLakeServiceClient
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Transporter received target coordinate request')
@@ -23,3 +23,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              "TransporterTargetCoordinates function executed successfully. Pass a transport site in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+def initialize_storage_account(storage_account_name, storage_account_key):
+    try:
+        global service_client
+
+        service_client = DataLakeServiceClient(
+            account_url="{}://{}.dfs.core.windows.net".format("https",storage_account_name),
+            credential=storage_account_key)
+    except Exception as e:
+        print(e)
