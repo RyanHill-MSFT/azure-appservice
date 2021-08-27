3cfd021cf1542211ea77921316dacb9f4d6c4e50
Ryan Hill <ryhill@microsoft.com>
2021-08-27 10:38:42 -0400

    lcars tricorder reorg

---
+++ LCARS/LCARS.Tricorder/LCARS.Tricorder/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using LCARS.Tricorder.Services;
+using LCARS.Tricorder.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LCARS.Tricorder
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
