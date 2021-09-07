25c72ce346a823f9cae7334a1202fb24848cdf7e
Ryan Hill <ryhill@microsoft.com>
2021-09-06 20:26:13 -0400

    added App.ts class

---
+++ LCARS/LCARS.Console.Desk.NuGamma/src/app.ts
@@ -0,0 +1,30 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+
+class App {
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+        this.routes();
+    }
+
+    private middleware(): void {
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private routes(): void {
+        let router = express.Router();
+        router.get("/", (req, res, next) => {
+            res.json({
+                message: "Hello World!"
+            });
+        });
+        this.express.set("port", process.env.PORT || 1337);
+        this.express.use("/", router);
+    }
+}
+
+export default new App().express;
--- LCARS/LCARS.Console.Desk.NuGamma/src/server.ts
+++ LCARS/LCARS.Console.Desk.NuGamma/src/server.ts
@@ -1,6 +1,42 @@
-import http = require('http');
-const port = process.env.port || 1337
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+import { debug } from "console";
+import * as http from "http";
+import { normalize } from "path";
+import app from "./app";
+
+const port = normalizePort(process.env.PORT || 1337);
+app.set("port", port);
+
+const server = http.createServer(app);
+server.listen(port);
+server.on("listening", onListening);
+server.on("error", onError);
+
+function normalizePort(value: string | number): number | string | boolean {
+    let port: number = (typeof value === 'string') ? parseInt(value, 10) : value;
+    if (isNaN(port)) return value;
+    else if (port >= 0) return port;
+    else return false;
+}
+
+function onError(error: NodeJS.ErrnoException): void {
+    if (error.syscall !== 'listen') throw error;
+    let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening(): void {
+    let addr = server.address();
+    let bind = (typeof addr === "string") ? `pipe ${addr}` : `port ${addr.port}`;
+    debug(`Listening on ${bind}`);
+}
