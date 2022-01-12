c1a694565eab2b493bfae240ad4acbf3cedfbd96
Ryan Hill <ryhill@microsoft.com>
2022-01-11 23:10:15 -0500

    updated to .net 6

--- LCARS/LCARS.Console.Interface.Delta/Pages/Index.cshtml.cs
+++ LCARS/LCARS.Console.Interface.Delta/Pages/Index.cshtml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Azure.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
 
 namespace LCARS.Console.Interface.Delta.Pages
 {
@@ -17,9 +20,28 @@ namespace LCARS.Console.Interface.Delta.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            var credential = new DefaultAzureCredential();
+            var token = credential.GetToken(
+                new Azure.Core.TokenRequestContext(
+                    new[] { "https://graph.microsoft.com/.default" }));
 
+            var accessToken = token.Token;
+            var graphServiceClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider((requestMessage) =>
+                {
+                    requestMessage.Headers
+                    .Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+
+                    return Task.CompletedTask;
+                }));
+
+            try
+            {
+                var users = await graphServiceClient.Users.Request().GetAsync();
+
+            }
         }
     }
 }
