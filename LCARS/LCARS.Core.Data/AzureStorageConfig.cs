c973a79e520bd004c6400e4e5e165e1aeaba6c7e
Ryan Hill <ryhill@microsoft.com>
2022-02-02 21:04:45 -0500

    storage model setup

---
+++ LCARS/LCARS.Core.Data/AzureStorageConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCARS.Core.Data
+{
+    public class AzureStorageConfig
+    {
+        public string AccountName { get; set; }
+        public string ContainerName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
